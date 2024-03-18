545c8343dabe88f50e7f0da8a475973e21b52ae8
Projeto Aulas Java <161389173+projetoaulasjava@users.noreply.github.com>
2024-03-18 20:20:12 -0300

    Segundo commit
    Adicionado código-fonte do início do projeto.

---
+++ Projeto_1_Impressao_&_Tipos_de_Dados/src/edu/etec/aula_1/code/Principal1.java
@@ -0,0 +1,67 @@
+package edu.etec.aula_1.code;
+
+import java.util.Scanner;
+
+public class Principal1 {
+
+	public static void main(String[] args) {
+		
+		/* 1
+		//___________________________________________
+		System.out.println("Hello, world!"); //Pula linha automaticamente e não permite a formatação de texto com especificadores de formato
+		System.out.printf("Olá, Mundo!"); //Permite a formatação de texto com especificadores de formato
+		
+		String nome = "James Gosling";
+		int data = 1991;
+		
+		System.out.printf(" %s criou o Java em %d...", nome, data); // James Gosling criou o Java
+		//___________________________________________
+		1 */
+		
+		/* 2
+		//___________________________________________
+		//Sequência de escape
+		System.out.println("Pulando \n duas \n linha.");
+		
+		// \n: Nova linha
+		// \": Aspas duplas
+		// \': Aspas simples
+		// \\: Barra invertida
+		
+		// \t: Tabulação horizontal
+		// \b: Backspace
+		// \f: Alimentação de formulário (form feed)
+		// \r: Retorno de carro (carriage return)
+		//___________________________________________
+		2 */
+		
+		/* 3
+		//___________________________________________
+		char generoAluno = 'F'; //Aspas Simples
+		char alunoMAtriculado = 'S'; //Aspas Simples
+		byte temperaturaAmbiente = -2;
+		byte idadeAluno = 18;
+		short populacaoMunicipal = 31000;
+		short populacaoMunicipal2 = 31_000;
+		int populacaoChina = 1500000000;
+		int populacaoChina2 = 1_500_000_000;
+		long populacaoMundo = 7500000000L; //Long
+		long populacaoMundo2 = 7_500_000_000l; //Long
+		float posicaoGeografica = 0.123456789123456F; //Float 
+		float precoProduto = 4_799.123_456_789_123_456f; //Float
+		double localizacaoFoguete = 0.123456789123456789D; //Double
+		double localizacaoFoguete2 = 1234.123456789123456789d; //Double
+		
+		boolean alunoAprovado = true; 
+		
+		String nome = "James Gosling";
+		//___________________________________________
+		3 */
+		
+		/* 4
+		 // Classes são PascalCase.
+		 // Variáveis são CamelCase.
+		4 */
+	}
+
+}
